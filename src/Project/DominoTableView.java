e53264d91f6994911e05a3831fdee70eac3b16fc
AnastasiaT0126 <72851610+AnastasiaT0126@users.noreply.github.com>
2020-12-09 12:11:30 +0200

    Domino table view

---
+++ src/Project/DominoTableView.java
@@ -0,0 +1,12 @@
+package Project;
+
+import javax.swing.JComponent;
+
+public class DominoTableView extends JComponent{
+	private DominoTable newTable;
+	
+	public DominoTableView(DominoTable table)
+	{
+		this.newTable = table;
+	}
+}
